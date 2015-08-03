2e79ca88e9a01a0fec2cb0fbb60653eff00d5381
muroslavko <myroslavdm@gmail.com>
2015-08-03 13:29:42 +0300

    Create

---
+++ Pars_LinkedIn/Pars_LinkedIn/Program.cs
@@ -0,0 +1,31 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pars_LinkedIn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var web = new HtmlWeb();
+            var doc = web.Load("https://ua.linkedin.com/in/antonvasilenko");
+            var name = doc.DocumentNode.SelectNodes("//span[@class='full-name']");
+            if (name != null)
+            {
+                foreach (var node in name)
+                {
+                    Console.WriteLine(node.InnerHtml);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Its null");
+            }
+            Console.ReadLine();
+        }
+    }
+}
